80d28afed568cd43933a3b321b7b78263c951490
wind-whl <73054988+wind-whl@users.noreply.github.com>
2024-04-08 16:58:18 +0800

    保存在合并前的本地更改

--- config.py
+++ config.py
@@ -7,12 +7,5 @@ class Config():
     topt = 6    #召回文本片段的数量
     maxlen = 128  #召回文本片段的长度
     topk = 5    #query召回的关键词数量
-    bert_path = r'E:\pretraing_models\torch\text2vec-base-chinese'
     recall_way = 'embed'  #召回方式 ,keyword,embed
-
-    #generator参数
-    model_name_or_path = r'E:\pretraing_models\torch\baichuan2-7B-Chat'  #llm模型位置
-    max_source_length = 767  #输入的最大长度
-    max_target_length = 256  #生成的最大长度
-    model_max_length = 1024  #序列最大长度
 
--- qa.py
+++ qa.py
@@ -1,26 +1,35 @@
-from generator import *
+from langchain_openai import ChatOpenAI
+from langchain import hub
+from retrievor import q_searching
+from config import Config
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnableLambda
+cfg = Config()
 
+llm = ChatOpenAI(model="gpt-4-32k",openai_api_base="",
+                 openai_api_key="")
+prompt = hub.pull("rlm/rag-prompt")
 
 
 if __name__=="__main__":
-    cfg = Config()
-    qg = QueryGenerate(cfg)
-    q_generate = qg.generate
-    q_chat = qg.chat
-    
-    
-    # query = '东南大学的现任校长是谁？'
-    # query = '叔本华信仰什么宗教？'
-    query = '戊戌变法中创建了什么报纸？'
-    # query = '华山派的开山鼻祖是谁？'
+
+    #query = '东南大学的现任校长是谁？'
+    #query = '叔本华信仰什么宗教？'
+    #query = '戊戌变法中创建了什么报纸？'
+    query ="华山派的开山鼻祖是谁？"
     # query = '家喻户晓的角色印第安纳·琼斯是由哪个演员扮演的？'
     # query = '有什么和"凿壁偷光"意思相近的词语？'
+    rag_chain = (
+            {"context": RunnablePassthrough() | RunnableLambda(q_searching), "question": RunnablePassthrough()}
+            | prompt
+            | llm
+            | StrOutputParser()
+    )
 
-    ####LLM+RAG#####
-    res = q_chat(query)
-    print(res)
+    for chunk in rag_chain.stream(query):
+        print(chunk, end="", flush=True)
 
-    ####LLM######
-    print('-----------------分割线-----------')
-    res = q_chat(query,use_retrievor=False)
-    print(res)
+
+
